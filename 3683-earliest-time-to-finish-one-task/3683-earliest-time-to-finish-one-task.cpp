8fbdd75c273c006234d1de353eae61f198a4425a
Shivam Upadhyay <shivamupadhyay2329@gmail.com>
2026-08-14 07:42:36 +0530

    Time: 0 ms (100%), Space: 38.5 MB (19.17%) - LeetHub

--- 3683-earliest-time-to-finish-one-task/3683-earliest-time-to-finish-one-task.cpp
+++ 3683-earliest-time-to-finish-one-task/3683-earliest-time-to-finish-one-task.cpp
@@ -2,15 +2,9 @@ class Solution {
 public:
     int earliestTime(vector<vector<int>>& tasks) {
         int ans=INT_MAX;
-        int n=tasks.size();
-        int m=tasks[0].size();
-        for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=0;j<m;j++){
-                sum+=tasks[i][j];
-            }
-            ans=min(ans,sum);
-        }   
+        for(auto task:tasks) {
+            ans=min(ans,task[0]+task[1]);
+        }
         return ans;
     }
 };
