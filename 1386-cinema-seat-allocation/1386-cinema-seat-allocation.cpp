9e9b41548d9e4838718f61ad344c2e928813f7f0
Shivam Upadhyay <shivamupadhyay2329@gmail.com>
2026-08-20 07:58:26 +0530

    Time: 39 ms (56.43%), Space: 69.3 MB (44.07%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
-        map<int,set<int>> mp;
-        for(auto seat:reservedSeats){
-            mp[seat[0]].insert(seat[1]);
+        unordered_map<int,vector<int>> mp;
+        for(auto &seat:reservedSeats){
+            mp[seat[0]].push_back(seat[1]);
         }
         int ans=2*n;
-        for(auto row:mp){
-            set<int> st=row.second;
-            bool left=true;
-            bool middle=true;
-            bool right=true;
-            for(int i=2;i<=5;i++){
-                if(st.find(i)!=st.end()){
-                    left=false;
-                    break;
-                }
-            }
-            for(int i=4;i<=7;i++){
-                if(st.find(i)!=st.end()){
-                    middle=false;
-                    break;
-                }
-            }
-            for(int i=6;i<=9;i++){
-                if(st.find(i)!=st.end()){
-                    right=false;
-                    break;
-                }
+        for(auto &[row,seats]:mp){
+            bool left=true,middle=true,right=true;
+            for(int x:seats){
+                if(x>=2 && x<=5) left=false;
+                if(x>=4 && x<=7) middle=false;
+                if(x>=6 && x<=9) right=false;
             }
             if(left && right){
                 continue;
