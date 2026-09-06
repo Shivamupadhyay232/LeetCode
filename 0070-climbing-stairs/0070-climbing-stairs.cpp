1fe9d608fb118ed750836a05fba1d99ede26855e
Shivam Upadhyay <shivamupadhyay2329@gmail.com>
2026-09-06 11:25:48 +0530

    Time: 0 ms (100%), Space: 7.7 MB (88.36%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int prev1=1;
+        int prev2=2;
+        int curr=0;
+        if(n==1){
+            return 1;
+        }
+        if(n==2){
+            return 2;
+        }
+        for(int i=2;i<n;i++){
+            curr=prev1+prev2;
+            prev1=prev2;
+            prev2=curr;
+        }
+        return curr;
+    }
+};
